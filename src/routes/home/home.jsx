45068034de146e310da29786d3e020956f27c771
Julio Salas <jscomic@gmail.com>
2022-11-07 23:01:41 -0500

    Home Reacts to google login

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useContext } from "react";
 
 import "./homeStyles.css";
 
@@ -7,38 +7,46 @@ import {
   createUserDocumentfromAuth,
 } from "../../utils/firebase";
 
+import { UserContext } from "../../context/userContext";
+
 function Home() {
+  //UserContext
+  const { setCurrentUser, currentUser } = useContext(UserContext);
+
+  //Log Google User
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentfromAuth(user);
+    await createUserDocumentfromAuth(user);
+    setCurrentUser(user);
   };
 
-  //after user has logged in we must get a property that says that the user has been logged in
-  //make a conditional that renders the list of tasks
-  //the nav bar also has a conditional
   return (
     <section className="home">
-      <div className="signIn">
-        <button onClick={logGoogleUser}>SignInWithGoogle</button>
-      </div>
-
-      <div className="sceneCounterContainer">
-        <h2> Has hecho 30 Tomas este mes</h2>
-      </div>
-      <div className="allTasksContainer">
-        <div className="task">
-          <div className="taskInfo">
-            <h2 className="taskNumber">PL079</h2>
-            <h3 className="episode">E02</h3>
-            <div className="taskStatus">Aprobado Alex</div>
+      {currentUser ? (
+        <div className="signIn">
+          <button onClick={logGoogleUser}>SignInWithGoogle</button>
+        </div>
+      ) : (
+        <>
+          <div className="sceneCounterContainer">
+            <h2> Has hecho 30 Tomas este mes</h2>
           </div>
-          <div>IMAGE</div>
-        </div>
-        <div className="taskOptions">
-          <button>Edit</button>
-          <button>Erase</button>
-        </div>
-      </div>
+          <div className="allTasksContainer">
+            <div className="task">
+              <div className="taskInfo">
+                <h2 className="taskNumber">PL079</h2>
+                <h3 className="episode">E02</h3>
+                <div className="taskStatus">Aprobado Alex</div>
+              </div>
+              <div>IMAGE</div>
+            </div>
+            <div className="taskOptions">
+              <button>Edit</button>
+              <button>Erase</button>
+            </div>
+          </div>
+        </>
+      )}
     </section>
   );
 }
